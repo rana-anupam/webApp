f05dd6e179ae7e9d5e7f64cee0d5133d3a3b4228
Apple <anupam.rana14071996@gmail.com>
2018-12-02 21:37:51 +0530

    make it better

--- app/cart/cartController.js
+++ app/cart/cartController.js
@@ -331,6 +331,70 @@ angular.module('hocketWebsite')
 			}
 		}
 
+
+
+
+
+
+
+
+
+
+
+
+
+$scope.checkoutRazor = function(){
+
+var options = {
+    "key": "rzp_live_SshfcZZqTDMQ4f",
+    //Key ID: rzp_live_SshfcZZqTDMQ4f, Key Secret: LpjvOsoRWeq79z01OJqa1pAa
+    "amount": "100", // 2000 paise = INR 20
+    "name": "Hocket Technologies Pvt. Ltd.",
+    "description": "Welcome to Hocket Family.",
+    "image": "./hocketLogo.jpg",
+    "handler": function (response){
+        alert(response.razorpay_payment_id);
+    },
+    "prefill": {
+        "name": "Gaurav Kumar",
+        "email": "test@test.com"
+    },
+    "notes": {
+        "address": "Hello World"
+    },
+    "theme": {
+        "color": "#F37254"
+    }
+};
+var rzp1 = new Razorpay(options);
+
+    rzp1.open();
+    e.preventDefault();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		
 
 }]);
