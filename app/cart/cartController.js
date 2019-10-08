03caec7a147a589edb122e647003669d002d44ee
Apple <anupam.rana14071996@gmail.com>
2019-10-08 12:21:07 +0530

    make it better

--- app/cart/cartController.js
+++ app/cart/cartController.js
@@ -169,15 +169,15 @@ angular.module('hocketWebsite')
 
 
 
-				else if( $scope.promoCode == 'HIMANSHU10'){
+				else if( $scope.promoCode == 'DEEPESH30'){
 					if($scope.cartSummaryRent < 500){
 						$scope.promocodeText = 'To apply this code minimum monthly rent should be Rs.500';
 						$scope.promocodeTextColor = 'red';
 					}
 					else{						
-					console.log("Apply HIMANSHU10");
-					$scope.cartSummaryDiscount = Math.ceil($scope.cartSummaryRent * 0.1);
-					$scope.promocodeText = 'Congratulations! you have received 10% off on your monthly rent';
+					console.log("Apply DEEPESH30");
+					$scope.cartSummaryDiscount = Math.ceil($scope.cartSummaryRent * 0.3);
+					$scope.promocodeText = 'Congratulations! you have received 30% off on your monthly rent';
 					$scope.promocodeTextColor = 'green';
 					$scope.updateCartSummary();
 					}
